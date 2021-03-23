cf79c4766bf038946a5da2675a2bdd7715e58df5
patricia <prmonday@gmail.com>
2021-03-23 13:40:15 +0000

    Exercise 1

---
+++ lib/encapsulation.rb
@@ -0,0 +1,56 @@
+# Exercise 1
+
+class Bank
+
+def initialize(date, amount)
+  @balance = balance
+  @date = date
+  @amount = amount
+
+end
+
+  def get_balance
+    @balance
+  end
+
+  def credit_account(date, amount)
+    puts @balance
+    puts @date
+    puts @amount
+  end
+
+  def debit_account(date, amount)
+    puts @amount
+  end
+
+    def print_statment
+      puts @balance
+      puts @date
+    end
+end
+
+class SecretDiary
+
+  def initialize
+    @key = key
+    @body = body
+  end
+
+  def lock
+    @key
+  end
+
+  def unlock
+    @key
+  end
+
+  def add_entry
+    @body
+  end
+
+  def get_entries
+    @body
+  end
+end
+
+#Exercise 2
